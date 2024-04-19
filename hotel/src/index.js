63947ddc0f2f509df2b4a0c6b0afa094cef4fe96
Qassim8 <mohammedalgasim123@gmail.com>
2024-04-19 08:17:06 +0200

    update routes

--- hotel/src/index.js
+++ hotel/src/index.js
@@ -2,21 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./all.min.css";
-// import { RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 import { Provider } from "react-redux";
 import store from "./store";
-// import { router } from "./router/routes";
+import { router } from "./router/routes";
 import reportWebVitals from "./reportWebVitals";
-import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
       <Provider store={store}>
-        {/* <RouterProvider router={router} /> */}
-        <App />
+        <RouterProvider router={router} />
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
